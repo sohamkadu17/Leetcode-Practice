0400b4fa873436505bb7efe12e4f78b4797f3cc8
Soham Kadu <kadusoham91@gmail.com>
2026-08-24 16:28:30 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() ==0 || s.length()%2 != 0){
+            return false;
+        }
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (!st.isEmpty() && ((st.peek() == '(' && s.charAt(i) == ')') || (st.peek() == '[' && s.charAt(i) == ']')
+                || (st.peek() == '{' && s.charAt(i) == '}'))){
+                    st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+
+        }
+        return st.isEmpty();
+    }
+}
+
